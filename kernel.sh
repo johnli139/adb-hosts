4b65352b5b598097f1ef4fc3a7fecd442e2f4806
johnli139 <53860653+johnli139@users.noreply.github.com>
2023-03-02 17:30:22 +0300

    Delete kernel.sh

--- kernel.sh
+++
@@ -1,5 +0,0 @@
-echo "net.core.rmem_max=26214400" >> /etc/sysctl.d/cloud-sysctl.conf
-echo "net.core.rmem_default=26214400" >> /etc/sysctl.d/cloud-sysctl.conf
-echo "net.core.wmem_max=26214400" >> /etc/sysctl.d/cloud-sysctl.conf
-echo "net.core.wmem_default=26214400" >> /etc/sysctl.d/cloud-sysctl.conf
-sysctl -p /etc/sysctl.d/cloud-sysctl.conf
